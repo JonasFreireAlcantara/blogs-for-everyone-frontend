8ab7bb182c88487e4052a73f352d3bf9c87c8159
Jonas Freire <jonasfreireperson@gmail.com>
2020-02-04 11:50:36 -0300

    initial commit and project setup

---
+++ src/Blog.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Blog() {
+  return (
+    <div className="Blog">
+      
+    </div>
+  );
+}
+
+export default Blog;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Blog from './Blog';
+
+ReactDOM.render(<Blog />, document.getElementById('root'));
